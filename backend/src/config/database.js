cff82036dff4c844980791db765689701551cb5f
Vikashcodez <chotuvikash6@gmail.com>
2026-05-08 10:59:16 +0530

    backend database.js customer table added

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -142,6 +142,21 @@ export const initializeDatabase = async () => {
       )
     `);
 
+    //customer table
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS customers (
+        customer_id SERIAL PRIMARY KEY,
+        customer_name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        phone_number VARCHAR(20) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        address JSONB NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
+
+
 
     
     console.log('✅ Users table created/verified');
